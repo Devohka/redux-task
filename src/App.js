2cf5c9060eae3b9a6bb2c578b185784cac88b5aa
Devohka <andr.fedak@gmail.com>
2025-01-31 20:13:56 +0200

    gh

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const activeTaskValue = useSelector((state) => {
+    return state.activeTask;
+  });
+  const completedTaskValue = useSelector((state) => {
+    return state.completedTask;
+  });
   const taskListValue = useSelector((state) => {
     return state.taskList;
   });
@@ -26,6 +32,7 @@ function App() {
       payload: {
         text: e.currentTarget.elements.textInput.value,
         id: createNumder(),
+
       }
     })
   };
@@ -33,37 +40,84 @@ function App() {
   const clickTask = (e) => {
     console.log(e.currentTarget.checked)
     if (e.currentTarget.checked === true) {
+      const idToActive = parseInt(e.target.closest("li")["id"]);
+      const newTaskList = taskListValue.filter(item => {
+
+        return item.id === idToActive;
+      });
+      console.log(newTaskList);
       dispatch({
         type: "OnTask",
-        payload: 1
-      })
+        payload: { num: 1, active: newTaskList }
+      });
+
+
+
     } else {
+
+      const idToActive = parseInt(e.target.closest("li")["id"]);
+      const newTaskList = taskListValue.filter(item => {
+
+        return item.id !== idToActive;
+      });
+      console.log(newTaskList);
       dispatch({
         type: "NoOnTask",
-        payload: 1
-      })
-    }
+        payload: { num: 1, active: newTaskList }
+      });
+    };
   };
 
   const deleteTask = (e) => {
     const idToDelete = parseInt(e.target.closest("li")["id"]);
+    const completedTask = [];
     const newTaskList = taskListValue.filter(item => {
-  
+
       return item.id !== idToDelete;
     });
     console.log(idToDelete);
+    completedTask.push(taskListValue.slice(idToDelete, 1));
     newTaskList.splice(idToDelete, 0);
+
+
     dispatch({
       type: "DeleteTask",
       payload: {
         taskList: newTaskList,
         completed: 1,
+        completedTaskEl: completedTask
       }
     })
 
     // remTellBook(newTellBook);
   };
 
+
+
+  const activeTaskList = () => {
+
+    dispatch({
+      type: "ActiveTask",
+      payload: activeTaskValue
+    });
+  };
+
+  const allTaskList = () => {
+
+    dispatch({
+      type: "AllTask",
+      payload: taskListValue
+    });
+  };
+
+
+  const completedTaskList = () => {
+
+    dispatch({
+      type: "CompletedTask",
+      payload: completedTaskValue
+    });
+  };
   return (
     <>
 
@@ -72,17 +126,22 @@ function App() {
       <p>Completed: {completedValue}</p>
 
       <form onSubmit={addTask}>
-        <input type="text" placeholder='Enter task text...' name='textInput'/>
+        <input type="text" placeholder='Enter task text...' name='textInput' />
         <button type='submit'>Add task</button>
       </form>
-
+      <div>
+        <h2>Filter by status</h2>
+        <button type='button' onClick={allTaskList}>All</button>
+        <button type='button' onClick={activeTaskList}>Active</button>
+        <button type='button' onClick={completedTaskList}>Completed</button>
+      </div>
 
       <ul>
         {taskListValue.map(text => {
           return (
             <>
               <li id={text.id} key={text.id}>
-                <InputAdd valueText={text.text} onclick={clickTask} ondelete={deleteTask}/>
+                <InputAdd valueText={text.text} onclick={clickTask} ondelete={deleteTask} />
               </li>
             </>
           );
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,11 @@ const state = {
     taskList: [{
         text: "Зробити планувальник",
         id: 0,
+
     }],
+    activeTask: [],
+
+    completedTask: [],
     active: 0,
     completed: 0,
 };
@@ -15,35 +19,63 @@ console.log(...state.taskList)
 
 
 const reduser = (state, action) => {
+    console.log(action.payload);
     if (action.type === "AddTask") {
-        console.log(action.payload);
+
         return {
             taskList: [...state.taskList, action.payload],
+            activeTask: state.activeTask,
             active: state.active,
             completed: state.completed
         };
-      
+
     } else if (action.type === "OnTask") {
         return {
             taskList: state.taskList,
-            active: state.active + action.payload,
+            activeTask: [...state.activeTask, ...action.payload.active],
+            active: state.active + action.payload.num,
             completed: state.completed
         };
-    }  else if (action.type === "NoOnTask") {
+    } else if (action.type === "NoOnTask") {
         return {
             taskList: state.taskList,
-            active: state.active - action.payload,
-            completed: state.completed 
+            activeTask: [...state.activeTask, ...action.payload.active],
+            active: state.active - action.payload.num,
+            completed: state.completed
         };
     } else if (action.type === "DeleteTask") {
         return {
             taskList: action.payload.taskList,
+            completedTask: [...state.completedTask, action.payload.completedTaskEl],
+            activeTask: state.activeTask,
             active: state.active,
             completed: state.completed + action.payload.completed
         };
-    }  
-   return state;
-   
+    } else if (action.type === "ActiveTask") {
+        return {
+            taskList: action.payload,
+            activeTask: state.taskList,
+            active: state.active,
+            completed: state.completed
+        };
+    } else if (action.type === "AllTask") {
+        return {
+            taskList: state.activeTask,
+            activeTask: state.taskList,
+            completedTask: state.taskList,
+            active: state.active,
+            completed: state.completed
+        };
+    } else if (action.type === "CompletedTask") {
+        return {
+            taskList: state.completedTask,
+            completedTask: state.taskList,
+            active: state.active,
+            completed: state.completed
+        };
+    }
+    return state;
+
 
 };
 const store = createStore(reduser, state, composeWithDevTools());
